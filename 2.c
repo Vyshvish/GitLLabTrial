dda9a85898e76887d83af4ac8426a8399a425309
Vyshvish <vyshnavir51@gmail.com>
2024-02-21 10:12:17 +0530

    TrendUpdate

---
+++ 2.c
@@ -0,0 +1,22 @@
+Hey
+Just a reminder that you need to study
+Internals are coming, externals dude!
+*_* 
+
+🤔️
+🤑️ paisa paisaaaa
+😴️ nidhe barthideeee
+😪️ i need a hand kerchief
+💩️ thattii 							
+🦋️ BUtTERFLYY  BUTTERFLY
+🍗️ Belluli Kabab	one more one more anbeku
+
+oh nallaaaaaaa
+neeenalllaaaaaaaaa
+Karimani Maaalikaaaaaaaa Neenallaaaaaaaaaaa
+Kariimanii Maalikaaa Rahullaaaaaaaaaaaaaaaa
+Rahullaaaaaa    Rahullaaaaaaaaaaaaa
+
+
+
+							#GIT_LAB_ANTHE
